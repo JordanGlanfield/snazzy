7a2138bb755ab02b511758c3f04484862f00eb79
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2024-03-21 23:12:03 +0000

    add text area to chat page

--- frontend/src/pages/Chat/Chat.jsx
+++ frontend/src/pages/Chat/Chat.jsx
@@ -1,6 +1,7 @@
 import { useShallow } from 'zustand/react/shallow'
 
 import styles from './Chat.module.css'
+import colours from '@/styles/colours.module.css'
 import { useChatStore } from '@/stores/chat.js'
 import Message from '@/components/Message/Message.jsx'
 
@@ -14,9 +15,14 @@ function Chat() {
     return (
         <>
             <div className={styles.wrapper}>
-                {messages.map((message) => (
-                    <Message key={message.text} message={message} />
-                ))}
+                <div className={styles.chatContainer}>
+                    {messages.map((message) => (
+                        <Message key={message.text} message={message} />
+                    ))}
+                </div>
+                <div className={styles.inputPanel}>
+                    <textarea className={`${styles.chatTextArea} ${colours.yellowBorder}`} />
+                </div>
             </div>
         </>
     )
